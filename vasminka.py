5a78355e8dca1062b91ff732a9bfbf27ec10b1d3
ArifOmer24 <arifomersicak37@gmail.com>
2021-10-30 15:33:25 +0300

    Last 3 Parts

---
+++ vasminka.py
@@ -0,0 +1,12 @@
+from turtle import *
+speed(5)
+bgcolor("black")
+color("greenyellow")
+pensize(5)
+for i in range(6):
+    left(60)
+    for i  in range(6):
+        forward(100)
+        left(60)
+hideturtle()
+done()
